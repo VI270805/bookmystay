1ef66ec2c5ecdcd2cdfac929ae67e44737417757
VI270805 <vishvjeet2708@gmail.com>
2026-03-10 14:22:37 +0530

    Base application setup

---
+++ src/uc1bookmystay.java
@@ -0,0 +1,15 @@
+public class uc1bookmystay {
+        public static void main(String[] args) {
+
+            // Welcome message
+            System.out.println("Welcome to Book My Stay - Hotel Booking Management System");
+
+            // Application version
+            System.out.println("Version : 1.0");
+
+            // System initialization message
+            System.out.println("System initialized successfully.");
+
+        }
+    }
+}
