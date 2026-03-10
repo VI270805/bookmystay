c3af882e060d68fb7cec7d8e2794650f721003a8
VI270805 <vishvjeet2708@gmail.com>
2026-03-10 14:40:27 +0530

    UC1: Add welcome message module

--- src/uc1bookmystay.java
+++ src/uc1bookmystay.java
@@ -1,15 +1,15 @@
-public class uc1bookmystay {
-        public static void main(String[] args) {
+public class UseCase1HotelBookingApp {
+git a
+    public static void main(String[] args) {
 
-            // Welcome message
-            System.out.println("Welcome to Book My Stay - Hotel Booking Management System");
+        // Welcome message
+        System.out.println("Welcome to Book My Stay - Hotel Booking Management System");
 
-            // Application version
-            System.out.println("Version : 1.0");
+        // Application version
+        System.out.println("Version : 1.0");
 
-            // System initialization message
-            System.out.println("System initialized successfully.");
+        // System initialization message
+        System.out.println("System initialized successfully.");
 
-        }
     }
 }
