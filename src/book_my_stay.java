7f333bb8ab36de9b2ac6a6be50499c5b1fe630d3
vs3980 <vishvjeet2708@gmail.com>
2026-03-17 14:56:30 +0530

    UC1: Add welcome message module

---
+++ src/book_my_stay.java
@@ -0,0 +1,13 @@
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        // Display welcome message
+        System.out.println("     Welcome to Book My Stay App     ");
+
+        // Display application details
+        System.out.println("Application Name : Hotel Booking Management System");
+        System.out.println("Version          : v1.0");
+
+        // End message
+        System.out.println("\nApplication started successfully. Exiting now...");
+    }
+}
